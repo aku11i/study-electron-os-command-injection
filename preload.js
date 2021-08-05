e62796d4631a29d7bed69ad557292e66a15abd4b
Shuhei Akutagawa <shuhei.akutagawa+github@gmail.com>
2021-08-05 21:18:58 +0900

    fix: Promiseを返す

--- preload.js
+++ preload.js
@@ -4,6 +4,6 @@ const { ipcRenderer, contextBridge } = require("electron/renderer");
 
 contextBridge.exposeInMainWorld("electron", {
   createNewDirectory: (name) => {
-    ipcRenderer.invoke("CREATE_NEW_DIRECTORY", name);
+    return ipcRenderer.invoke("CREATE_NEW_DIRECTORY", name);
   },
 });
